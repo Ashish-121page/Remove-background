953763c11264b405ea59815478b45027942e4f51
Ashish-121page <124762142+Ashish-121page@users.noreply.github.com>
2024-01-04 11:23:49 +0530

    Update app.py
    Removing Download Modal

--- app.py
+++ app.py
@@ -2,27 +2,12 @@ from flask import Flask, request, jsonify
 from rembg import remove
 import os
 from werkzeug.utils import secure_filename
-import requests
+import mysql.connector
 
-# Set environment variable for model
+# import os
 os.environ["OPENBLAS_L2_SIZE"] = "262144"
 
 app = Flask(__name__)
-
-# Function to download u2net.onnx if not present
-def download_u2net_model():
-    model_path = '/workspace/.u2net/u2net.onnx'  # Update this path
-    model_url = 'https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net.onnx'
-
-    if not os.path.exists(model_path):
-        print("Downloading u2net.onnx model...")
-        response = requests.get(model_url)
-        with open(model_path, 'wb') as file:
-            file.write(response.content)
-        print("Model downloaded.")
-
-# Call the download function on app start
-download_u2net_model()
 
 # Directory to store received files
 received_files_dir = 'static/received_files'
@@ -34,22 +19,43 @@ processed_files_dir = 'static/processed_files'
 os.makedirs(processed_files_dir, exist_ok=True)
 app.config['PROCESSED_FILES'] = processed_files_dir
 
+# MySQL database connection
+# db = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="",
+#     database="python"
+# )
+
 @app.route('/')
 def index():
     return "Welcome to the Image Processing Service!"
 
+
 @app.route('/process', methods=['POST'])
 def process_image():
     if 'file' not in request.files:
-        return jsonify({"error": "No file part", "response_code": 400}), 400
+        return jsonify({"error": "No file part","response_code": 400}), 400
 
     if 'api_key' not in request.form:
-        return jsonify({"error": "No api_key provided", "response_code": 400}), 400
+        return jsonify({"error": "No api_key provided","response_code": 400}), 400
 
-    file = request.files['file']
+    if 'image_type' not in request.form:
+        file = request.files['file']
+    else:
+        file = request.files['file']
+
 
     customFilename = request.form['Myfilename']
     api_key = request.form['api_key']
+
+    # # Validate the api_key
+    # cursor = db.cursor()
+    # query = "SELECT * FROM api_keys WHERE api_key = %s"
+    # cursor.execute(query, (api_key,))
+    # result = cursor.fetchone()
+    # if result is None:
+    #     return jsonify({"error": "Invalid api_key","response_code": 400}), 401
 
     if file:
         # Save the received file with original name and extension
@@ -63,11 +69,12 @@ def process_image():
         file_path = os.path.join(app.config['RECEIVED_FILES'], og_filename)
         file.save(file_path)
 
-        # Perform background removal
+        # Perform other tasks
         input_data = open(file_path, 'rb').read()
         output_data = remove(input_data)
 
-        # Save the processed file
+        # Save the processed file with original name and extension
+        # processed_filename = file.filename
         processed_filename = filename.split('.')[0] + '.png'
         output_path = os.path.join(app.config['PROCESSED_FILES'], processed_filename)
         with open(output_path, 'wb') as f:
@@ -76,7 +83,13 @@ def process_image():
         # Generate URL for the processed file
         file_url = request.url_root + app.config['PROCESSED_FILES'] + '/' + processed_filename
 
-        return jsonify({"url": file_url, "filename": processed_filename, "status": "success", "response_code": 200})
+        # Store data in MySQL
+        # cursor = db.cursor()
+        # insert_query = "INSERT INTO processed_files (filename, url) VALUES (%s, %s)"
+        # cursor.execute(insert_query, (processed_filename, file_url))
+        # db.commit()
+
+        return jsonify({"url": file_url, "filename": processed_filename, "status": "success","response_code": 200})
 
 # # Ensure this script is run as the main program and not as a module
 # if __name__ == '__main__':
